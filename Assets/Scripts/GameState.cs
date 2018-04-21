7a9cbf4db15697faa0a0504e78c7f2ce6f473a37
Aggrathon <antonbjo@gmail.com>
2018-04-21 15:46:42 +0300

    Hero Movements

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public static GameState instance { get; protected set; }
+	public HeroController heroes;
+	public float startDelay = 1.5f;
+
+	int turns;
+
+	void Start ()
+	{
+		instance = this;
+		StartCoroutine(DelayedStart());
+		turns = 0;
+	}
+
+	IEnumerator DelayedStart()
+	{
+		yield return new WaitForSeconds(startDelay);
+		PlayerTurn();
+	}
+
+	public void EnemyTurn()
+	{
+		turns++;
+		//TODO: Enemy turn
+		PlayerTurn();
+	}
+
+	public void PlayerTurn()
+	{
+		heroes.StartTurn();
+	}
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
 
 	public SpriteRenderer sprite;
+	public int cooldown = 1;
+	int cd;
 
 	void Start () {
 		if (sprite == null)
 			sprite = GetComponent<SpriteRenderer>();
-		GetComponentInParent<HeroTiles>().SpawnHero(transform);
+		GetComponentInParent<HeroController>().SpawnHero(this);
+		cd = 0;
+	}
+
+	public void DoAction(int lane)
+	{
+		//TODO: Hero Actions
+		cd = cooldown;
+		GameState.instance.EnemyTurn();
+	}
+
+	public bool CanMove()
+	{
+		if (cd > 0)
+		{
+			cd--;
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 }
